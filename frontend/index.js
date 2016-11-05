9c027043459627cf9647b68544e0c889e15bee64
Tom Pridham <pridham.tom@gmail.com>
2016-11-05 10:18:47 -0600

    reverting change for dm git lecture

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,7 @@
 `use strict`;
-// this is a better comment
+
 import React from 'react';
-import luke from 'Luke'
 import ReactDOM from 'react-dom';
-import Test from 'test'
 import {IndexRoute, browserHistory, Router, Route} from 'react-router';
 import App from './components/App';
 import Boggle from './components/Boggle'
@@ -15,7 +13,6 @@ require('bootstrap/dist/js/bootstrap.min');
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <Route path="whatever">test</Route>
             <IndexRoute component={Boggle}/>
             <Route path="*" component={App}/>
         </Route>
