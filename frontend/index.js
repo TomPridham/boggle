c2d4a6774e17e0e1ddb2d7ff2d592dcb924195d8
Tom Pridham <pridham.tom@gmail.com>
2016-11-05 09:44:55 -0600

    added some junk

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,9 @@
 `use strict`;
 // this is a better comment
 import React from 'react';
+import luke from 'Luke'
 import ReactDOM from 'react-dom';
+import Test from 'test'
 import {IndexRoute, browserHistory, Router, Route} from 'react-router';
 import App from './components/App';
 import Boggle from './components/Boggle'
@@ -13,6 +15,7 @@ require('bootstrap/dist/js/bootstrap.min');
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
+            <Route path="whatever">test</Route>
             <IndexRoute component={Boggle}/>
             <Route path="*" component={App}/>
         </Route>
